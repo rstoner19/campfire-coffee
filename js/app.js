1767affbf0d7d9852924715cf7b7899a8c5c7ae6
Rick Stoner <stonerrick19@gmail.com>
2016-01-13 19:12:41 -0800

    damn css, I don't know what I'm doing

--- js/app.js
+++ js/app.js
@@ -1,5 +1,5 @@
 
-// var storeData = [[0],['Hours','Customers','Cups','Beans for Cups','Beans for Bags','Lbs for Coffee'];
+var storeData = [[0],['Hours','Customers','Cups','Beans for Cups','Beans for Bags','Lbs for Coffee']];
 var count = 1;
 var hours = ['6:00am','7:00am','8:00am','9:00am','10:00am','11:00am','12:00pm','1:00pm','2:00pm','3:00pm','4:00pm','5:00pm','6:00pm','7:00pm','8:00pm'];
 function coffeeShops(name, minCust,maxCust,cups, pounds){
